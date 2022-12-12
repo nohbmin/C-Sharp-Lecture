d3b9f4ccfc94cb2db1a7a1df79a43541e76ec842
mol1986@naver.com <kileab@naver.com>
2022-12-12 21:47:56 +0900

    (2022.12.12) 박싱과 언박싱

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -2,60 +2,97 @@
 
 namespace ConsoleGame
 {
-    class Monster
+    class Solution
     {
-        // 기본 접근 지정자
-        // 필드에서 private로 설정됩니다.
-        public int attack;
-        public string name;
-
-        public void Defense()
+        public void Function()
         {
-            Console.WriteLine("방어");
+            Console.WriteLine("Function");
+        }
+
+        public int Calculator()
+        {
+            // 반환하는 메소드의 자료형과 반환하는 값의 자료형이
+            // 일치해야 됩니다.
+            return 100;
+        }
+
+        public void Swap(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void Post()
+        {
+            Console.WriteLine("Static Post");
         }
     }
-    
+
     internal class Program
-    {
-        // static void Main(string[ ] args)
-        // 프로그램 실행되는 진입점
-        // Main() 함수 안에서 실행해야 프로그램이 실행됩니다.
+    {       
         static void Main(string[] args)
         {
-            // 값 타입
-            // 변수의 선언과 동시에 값을 할당할 수 있으며,
-            // 스택 메모리에 생성됩니다.
-
-            // 참조 타입
-            // new 키워드를 사용해서 객체를 초기화한 후 
-            // 힙에 할당된 메모리를 스택에서 참조하여 사용하는 타입입니다.
+            #region 박싱(Boxing)
+            /*
+            // 값 형식을 참조 형식으로 변환해주는 과정입니다.
 
             int value = 10;
-            value = 20;
 
-            string name = "kimgeumsoo";
-            int [] array = new int[5];
+            // 참조    값
+            // [ ] <- 10
+            // 암묵적으로 형 변환이 됩니다.
 
-            name = "kang";
+            // object가 값 타입보다 상위의 자료형이기 때문에
+            // 암묵적으로 형 변환이 가능합니다.
+            object obj = value;
 
-            Monster monster = new Monster();
-            monster.attack = 10;
-            monster.name = "Goblin";
-            monster.Defense();
+            obj = 300;
 
+            // 박싱을 하는 과정에서 추가적인 메모리가 발생합니다.
+            Console.WriteLine("obj의 값 : " + obj);
+            Console.WriteLine("value의 값 : " + value);
+            */
+            #endregion
 
-            array[0] = 10;
-            array[1] = 20;
-            array[2] = 30;
-            array[3] = 40; 
-            array[4] = 50;
+            #region 언박싱(UnBoxing)
+            /*
+            // 참조 타입을 값 타입으로 변환하는 과정입니다.
 
-            Console.WriteLine(name);
-            Console.WriteLine(array[0]);
-            Console.WriteLine(array[1]);
-            Console.WriteLine(array[2]);
-            Console.WriteLine(array[3]);
-            Console.WriteLine(array[4]);
+            // data 변수가 값 타입이므로, obj 참조 타입보다
+            // 하위 자료형이기 때문에 명시적으로 형 변환을 해주어야 합니다.
+            int data = (int)obj;
+
+            // 언박싱 변환 시 스택메모리에 언박싱된 무명객체가 생성되며,
+            // 이 무명객체를 다시 해당 객체에 대입하는 원리입니다.
+            Console.WriteLine("data의 값 : " + data);
+            
+            object [ ] dateTable = new object[5];
+
+            dateTable[0] = 10;
+            dateTable[1] = "Sword";
+            dateTable[2] = 36.5f;
+            dateTable[3] = 'A';
+            dateTable[4] = true;
+            */
+            #endregion
+
+            Solution.Post();
+
+            int value1 = 10;
+            int value2 = 20;
+
+            Solution solution = new Solution();
+            solution.Function();
+
+            solution.Swap(value1, value2);
+
+            Console.WriteLine("value1의 값 : " + value1);
+            Console.WriteLine("value2의 값 : " + value2);
+
+            Console.WriteLine(solution.Calculator());
+
         }
+
     }
 }
---
+++ ConsoleGame/SecondLecture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    internal class SecondLecture
+    {
+        class Monster
+        {
+            // 기본 접근 지정자
+            // 필드에서 private로 설정됩니다.
+            public int attack;
+            public string name;
+
+            public void Defense()
+            {
+                Console.WriteLine("방어");
+            }
+        }
+
+        // 값 타입
+        // 변수의 선언과 동시에 값을 할당할 수 있으며,
+        // 스택 메모리에 생성됩니다.
+
+        #region 참조 타입
+        /*
+        // new 키워드를 사용해서 객체를 초기화한 후 
+        // 힙에 할당된 메모리를 스택에서 참조하여 사용하는 타입입니다.
+
+        int value = 10;
+        value = 20;
+
+        string name = "kimgeumsoo";
+        int [] array = new int[5];
+
+        name = "kang";
+
+        Monster monster = new Monster();
+        monster.attack = 10;
+        monster.name = "Goblin";
+        monster.Defense();
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40; 
+        array[4] = 50;
+
+        Console.WriteLine(name);
+        Console.WriteLine(array[0]);
+        Console.WriteLine(array[1]);
+        Console.WriteLine(array[2]);
+        Console.WriteLine(array[3]);
+        Console.WriteLine(array[4]);
+      */
+        #endregion
+
+    }
+}
